ebed2794b0b5294a7d47c15de90ebbce03c421c0
ziginsider <ziginsider@gmail.com>
2018-05-06 12:00:23 +0300

    add LoaderCallback

--- app/src/main/java/io/github/ziginsider/epam_laba_16/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_16/MainActivity.kt
@@ -1,12 +1,28 @@
 package io.github.ziginsider.epam_laba_16
 
+import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.LoaderManager
+import android.support.v4.content.CursorLoader
+import android.support.v4.content.Loader
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateLoader(id: Int, args: Bundle?) = CursorLoader(this, CONTENT_URI,
+            DEFAULT_REQUEST, null, null, null)
+
+    override fun onLoadFinished(loader: Loader<Cursor>?, data: Cursor?) {
+        //TODO adapter.swapCursor(data)
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>?) {
+        //TODO adapter.swapCursor(null)
+    }
+
+
 }
